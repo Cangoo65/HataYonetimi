5a2d05ef9b8d617e2d6126e36eb70cb8aff9bf47
Cangoo65 <caneraytekin06@hotmail.com>
2024-01-28 20:15:36 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿try
+{
+    var a = "Ali";
+    Console.WriteLine(int.Parse(a));
+    Console.WriteLine("İşlem Başarı İle Sonuçlandı");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("İşlemde " + ex.Message + " hatası oluştu.");
+}
+finally
+{
+    Console.WriteLine("İşlem sona erdi");
+    Console.ReadLine();
+}
